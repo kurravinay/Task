3a6ada62e1aadf68b89d8925ac9d906cfc063f7e
kurravinay <vinay.kurra@gmail.com>
2021-05-12 12:06:49 +0530

    Add files via upload

---
+++ admin/products/producer.py
@@ -0,0 +1,12 @@
+import pika, json
+
+params = pika.URLParameters('amqps://flynhnqj:EpVX2LXRTBUsv8e6AYE6aIFQQESpCZ5R@puffin.rmq2.cloudamqp.com/flynhnqj')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
